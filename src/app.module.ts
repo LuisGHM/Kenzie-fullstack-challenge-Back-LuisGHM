c2c3d68d99882cd74dfc89aa068db8d41dc01ec0
Luis <lgmarchioreh@gmail.com>
2024-01-24 10:02:23 -0300

    FEAT(contacts): Nest CLI generated necessary files for API endpoints to create, read, update, and delete contacts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule } from './modules/clients/clients.module';
+import { ContactsModule } from './modules/contacts/contacts.module';
 
 @Module({
-  imports: [ClientsModule],
+  imports: [ClientsModule, ContactsModule],
   controllers: [],
   providers: [],
 })
